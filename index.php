7b8d185111a2e3476156d73f61c5df3128fd541b
Naicen-dev <hamid1taymuree@gmail.com>
2026-02-23 14:57:59 +0100

    fix: Remove duplicate stylesheet link from index.php.

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <title>Stadtbibliothek Buxtehude</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
